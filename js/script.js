0ca61bf81cbcdbd61e386a3c9aa99f3070670a77
shutterstacks <shutterstacks@gmail.com>
2020-10-07 10:22:25 -0400

    decay rate added for updates

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@
 
 
 var countNum = [];
-
 var StringCounter = "";
 var number = 1;
 var d = document;
@@ -30,7 +29,7 @@ var leftPane = d.getElementById("left")
 //name[7]=string id | name[8]=string name
 //name[9]=string description | name[10]=tick
 //name[11]=string cost | name[12]=string number
-//name[13]=boolean
+//name[13]=boolean | name[14]=decay rate
 //
 //upgrade[0] = blood
 //
@@ -46,6 +45,7 @@ var messageChecker = new Array(1000);
 data.upgradeInstance.fill(false);
 messageChecker.fill(false);
 
+//////
 
 function count(number) {
     data.counter += number;
@@ -77,11 +77,7 @@ function count(number) {
 
     counterText.innerHTML = StringCounter;
 }
-
-heart.addEventListener("click", ()=> {
-   count(number)
-})
-
+//////
 
 function checkMessage(text, check, num) {
     if(check === false) {
@@ -89,12 +85,13 @@ function checkMessage(text, check, num) {
        setTimeout(()=>{
         d.getElementById("strip" + num).style = "";
        }, 500)
-        console.log(check);
          return true;
     }
 }
 
-function addUpgrade(cost, increaseRate, number, name, reference, description, tick, dataPosition) {
+//////
+
+function addUpgrade(cost, increaseRate, number, name, reference, description, tick, dataPosition, decayRate) {
 
     if(!data.upgradeInstance[dataPosition]) {
 
@@ -115,17 +112,24 @@ function addUpgrade(cost, increaseRate, number, name, reference, description, ti
     d.getElementById(reference + "UpgradeNum"),
     d.getElementById(reference + "Upgrade"),
     increaseRate, reference + "Upgrade", "Pipe Valve", "Pipes blood every 1s", tick,
-    reference+ "UpgradeCost", reference+ "UpgradeNum", false]
+    reference+ "UpgradeCost", reference+ "UpgradeNum", false, decayRate]
 
     setInterval(()=>{
         count(data.upgrade[dataPosition][0])
     }, data.upgrade[dataPosition][10])
 
+    setInterval(()=>{
+        if( data.upgrade[dataPosition][2] > 0)
+        data.upgrade[dataPosition][2]--
+        data.upgrade[dataPosition][0]--
+        data.upgrade[dataPosition][1] -= data.upgrade[dataPosition][6]
+    }, decayRate * 1000)
+
         data.upgrade[dataPosition][5].addEventListener("click", ()=> {
             if(data.counter > data.upgrade[dataPosition][1]) {
     
                 data.upgrade[dataPosition][2]++
-                data.upgrade[dataPosition][0] += 1
+                data.upgrade[dataPosition][0]++
                 data.counter -= data.upgrade[dataPosition][1]
                 data.upgrade[dataPosition][1] += data.upgrade[dataPosition][6]
             }
@@ -136,6 +140,8 @@ function addUpgrade(cost, increaseRate, number, name, reference, description, ti
     data.upgradeInstance[dataPosition] = true
 }}
 
+//////
+
 function updateData() {
     for(x in data.upgrade) {
         data.upgrade[x][3].innerHTML = "Cost: " + data.upgrade[x][1]
@@ -143,7 +149,7 @@ function updateData() {
     }
 }
 
-function buyUpdate() {}
+//////
 
 function checkUpgradeCost() {
     for(x in data.upgrade) {
@@ -157,16 +163,17 @@ function checkUpgradeCost() {
         data.upgrade[x][5].style.borderColor = "black"
         data.upgrade[x][5].classList.remove("active");
     }
-}
-}
+}}
 
+//////
 
+heart.addEventListener("click", ()=> {
+    count(number)
+ })
 
+//////
 
-
-
-
-let decayTick = 200
+let decayTick = 800
 let tick = 100;
 let bloodTick = 500;
 let frame = 0;
@@ -181,11 +188,11 @@ frame++
 switch (data.counter) {
     case 10:
     messageChecker[0] = checkMessage("Energy received x10 - Blood Valve acquired", messageChecker[0], 0);
-    addUpgrade(10, 5, 0, "Blood Valve", "valve", "Pumps blood every 1 s", 1000, 0);
+    addUpgrade(10, 5, 0, "Blood Valve", "valve", "Pumps blood every 1 s<br>Decays every 5 s", 1000, 0, 5);
     break;
     case 20:
     messageChecker[1] = checkMessage("Energy received x20 - Blood Pipe acquired", messageChecker[1], 1);
-    addUpgrade(20, 10, 0, "Blood Pipe", "pipe", "Pipes blood every 0.5 s", 500, 1);
+    addUpgrade(20, 10, 0, "Blood Pipe", "pipe", "Pipes blood every 0.5 s<br>Decays every 10 s", 500, 1, 10);
     default:break;
 }
 
